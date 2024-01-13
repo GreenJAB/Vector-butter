00616c406a4c90987ad8df777689e63896324e80
joshua <joshua@baikie.org>
2024-01-13 17:25:03 +1100

    setting up base prefabs

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    public meal mealPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
